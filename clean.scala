474f0bc3ffacf2dc7b0dfcb5d40e58601e9a4ac7
wangtuntun <wtt561111@163.com>
2016-05-10 21:34:52 +0800

    添加过滤操作

--- clean.scala
+++ clean.scala
@@ -23,6 +23,7 @@ object clean {
     val songs_pair=songs_split.map( x=>( (x(0)),(x(1),x(2),x(3),x(4),x(5)) ) )
     val user_action_pair=user_action_split.map( x=>((x(1)),(x(0),x(2),x(3),x(4)))  )
     val join=user_action_pair.join(songs_pair)
+    //下面是对join后的进行清洗
 
     sc.stop();
 
