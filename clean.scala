28e08e39d6775e02536848d9e461230ee0aa7d2c
wangtuntun <wtt561111@163.com>
2016-05-11 21:25:21 +0800

    将user_action里面的DS由日期字符串转为数字（距离20150301第几天）

--- clean.scala
+++ clean.scala
@@ -23,8 +23,16 @@ object clean {
     val songs_pair=songs_split.map( x=>( (x(0)),(x(1),x(2),x(3),x(4),x(5)) ) )
     val user_action_pair=user_action_split.map( x=>((x(1)),(x(0),x(2),x(3),x(4)))  )
     val join=user_action_pair.join(songs_pair)
-    val filter=join.filter( x=>  (x._2._1._3  )<( x._2._2._1 )        )
-
+    //去掉播放时间小于出版时间的记录
+    val filter=join.filter( x=>  (x._2._1._4  )<( x._2._2._2 ) )
+    //将user_action里面的日期字符串转为数字序列（播放时间-出版日期）
+    val start_date="20150301"
+    val user_action_date_to_int=user_action_split.map{x=>
+      val play_date=x(4)
+      val days=play_date.toInt - start_date.toInt
+      (x(0),x(1),x(2),x(3),days)
+    }
+    user_action_date_to_int.saveAsTextFile("/home/wangtuntun/AliMusic/Data/user_action_DS_int")
     sc.stop();
 
   }
